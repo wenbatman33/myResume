86f3226203a3f87f9fc14cc9aebc8e1332592724
wenbatman33 <wenbatman33@gmail.com>
2017-03-02 00:24:13 +0800

    update_1
    update_1

--- project/pega/Item_list.php
+++ project/pega/Item_list.php
@@ -15,7 +15,7 @@
                 <![CDATA[uploadfile/001.jpg]]>
             </小圖連結>
             <大圖連結>
-                <![CDATA[uploadfile/l_1208331127.jpg]]>
+                <![CDATA[uploadfile/_l_001.jpg]]>
             </大圖連結>
             <Name>
                 <![CDATA[S6]]>
@@ -41,7 +41,7 @@
                 <![CDATA[uploadfile/002.jpg]]>
             </小圖連結>
             <大圖連結>
-                <![CDATA[uploadfile/l_1208282883.jpg]]>
+                <![CDATA[uploadfile/_l_002.jpg]]>
             </大圖連結>
             <Name>
                 <![CDATA[U1]]>
@@ -67,7 +67,7 @@
                 <![CDATA[uploadfile/003.jpg]]>
             </小圖連結>
             <大圖連結>
-                <![CDATA[uploadfile/l_1208265121.jpg]]>
+                <![CDATA[uploadfile/_l_003.jpg]]>
             </大圖連結>
             <Name>
                 <![CDATA[U1]]>
@@ -93,7 +93,7 @@
                 <![CDATA[uploadfile/004.jpg]]>
             </小圖連結>
             <大圖連結>
-                <![CDATA[uploadfile/l_1208282907.jpg]]>
+                <![CDATA[uploadfile/_l_004.jpg]]>
             </大圖連結>
             <Name>
                 <![CDATA[Lamborghini]]>
@@ -119,7 +119,7 @@
                 <![CDATA[uploadfile/005.jpg]]>
             </小圖連結>
             <大圖連結>
-                <![CDATA[uploadfile/l_1208282814.jpg]]>
+                <![CDATA[uploadfile/_l_005.jpg]]>
             </大圖連結>
             <Name>
                 <![CDATA[R2]]>
@@ -145,7 +145,7 @@
                 <![CDATA[uploadfile/006.jpg]]>
             </小圖連結>
             <大圖連結>
-                <![CDATA[uploadfile/l_1208264682.jpg]]>
+                <![CDATA[uploadfile/_l_006.jpg]]>
             </大圖連結>
             <Name>
                 <![CDATA[Ecobook]]>
@@ -171,7 +171,7 @@
                 <![CDATA[uploadfile/007.jpg]]>
             </小圖連結>
             <大圖連結>
-                <![CDATA[uploadfile/l_1208264703.jpg]]>
+                <![CDATA[uploadfile/_l_007.jpg]]>
             </大圖連結>
             <Name>
                 <![CDATA[Ecobook]]>
@@ -197,7 +197,7 @@
                 <![CDATA[uploadfile/008.jpg]]>
             </小圖連結>
             <大圖連結>
-                <![CDATA[uploadfile/l_1208263789.jpg]]>
+                <![CDATA[uploadfile/_l_008.jpg]]>
             </大圖連結>
             <Name>
                 <![CDATA[R2]]>
@@ -223,7 +223,7 @@
                 <![CDATA[uploadfile/009.jpg]]>
             </小圖連結>
             <大圖連結>
-                <![CDATA[uploadfile/l_1208282983.jpg]]>
+                <![CDATA[uploadfile/_l_001.jpg]]>
             </大圖連結>
             <Name>
                 <![CDATA[Lamborghini]]>
@@ -249,7 +249,267 @@
                 <![CDATA[uploadfile/010.jpg]]>
             </小圖連結>
             <大圖連結>
-                <![CDATA[uploadfile/l_1208265036.jpg]]>
+                <![CDATA[uploadfile/_l_002.jpg]]>
+            </大圖連結>
+            <Name>
+                <![CDATA[W1]]>
+            </Name>
+            <Categories>
+                <![CDATA[Notebook]]>
+            </Categories>
+            <DATE>
+                <![CDATA[Beautiful Utilitarian]]>
+            </DATE>
+            <Description>
+                <![CDATA[The W1 notebook features military aviation grade carbon fiber on its front cover. The material’s strength and lightweight characteristics make it ideal for a product intended for mobility. The design of this notebook uses strong common shapes to harmonize with the rigidity of carbon fiber.]]>
+            </Description>
+        </Product>
+        <Product>
+            <主標>
+                <![CDATA[S6]]>
+            </主標>
+            <副標>
+                <![CDATA[Notebook]]>
+            </副標>
+            <小圖連結>
+                <![CDATA[uploadfile/001.jpg]]>
+            </小圖連結>
+            <大圖連結>
+                <![CDATA[uploadfile/_l_001.jpg]]>
+            </大圖連結>
+            <Name>
+                <![CDATA[S6]]>
+            </Name>
+            <Categories>
+                <![CDATA[Notebook]]>
+            </Categories>
+            <DATE>
+                <![CDATA[A Touch of Luxury]]>
+            </DATE>
+            <Description>
+                <![CDATA[]]>
+            </Description>
+        </Product>
+        <Product>
+            <主標>
+                <![CDATA[U1]]>
+            </主標>
+            <副標>
+                <![CDATA[Notebook]]>
+            </副標>
+            <小圖連結>
+                <![CDATA[uploadfile/002.jpg]]>
+            </小圖連結>
+            <大圖連結>
+                <![CDATA[uploadfile/_l_002.jpg]]>
+            </大圖連結>
+            <Name>
+                <![CDATA[U1]]>
+            </Name>
+            <Categories>
+                <![CDATA[Notebook]]>
+            </Categories>
+            <DATE>
+                <![CDATA[]]>
+            </DATE>
+            <Description>
+                <![CDATA[]]>
+            </Description>
+        </Product>
+        <Product>
+            <主標>
+                <![CDATA[U1]]>
+            </主標>
+            <副標>
+                <![CDATA[Notebook]]>
+            </副標>
+            <小圖連結>
+                <![CDATA[uploadfile/003.jpg]]>
+            </小圖連結>
+            <大圖連結>
+                <![CDATA[uploadfile/_l_003.jpg]]>
+            </大圖連結>
+            <Name>
+                <![CDATA[U1]]>
+            </Name>
+            <Categories>
+                <![CDATA[Notebook]]>
+            </Categories>
+            <DATE>
+                <![CDATA[]]>
+            </DATE>
+            <Description>
+                <![CDATA[]]>
+            </Description>
+        </Product>
+        <Product>
+            <主標>
+                <![CDATA[Lamborghini]]>
+            </主標>
+            <副標>
+                <![CDATA[Notebook]]>
+            </副標>
+            <小圖連結>
+                <![CDATA[uploadfile/004.jpg]]>
+            </小圖連結>
+            <大圖連結>
+                <![CDATA[uploadfile/_l_004.jpg]]>
+            </大圖連結>
+            <Name>
+                <![CDATA[Lamborghini]]>
+            </Name>
+            <Categories>
+                <![CDATA[Notebook]]>
+            </Categories>
+            <DATE>
+                <![CDATA[Embodying Classic Brand]]>
+            </DATE>
+            <Description>
+                <![CDATA[To capture the spirit and power of Lamborghini, we studied their line of cars extensively. The fine detailing of this notebook is on par with an artfully constructed sports car. The visual language of the air vents emulates a wheel cover and the same bold stitches on a car seat have been implemented on the palm rest.]]>
+            </Description>
+        </Product>
+        <Product>
+            <主標>
+                <![CDATA[R2]]>
+            </主標>
+            <副標>
+                <![CDATA[Notebook]]>
+            </副標>
+            <小圖連結>
+                <![CDATA[uploadfile/005.jpg]]>
+            </小圖連結>
+            <大圖連結>
+                <![CDATA[uploadfile/_l_005.jpg]]>
+            </大圖連結>
+            <Name>
+                <![CDATA[R2]]>
+            </Name>
+            <Categories>
+                <![CDATA[Notebook]]>
+            </Categories>
+            <DATE>
+                <![CDATA[]]>
+            </DATE>
+            <Description>
+                <![CDATA[]]>
+            </Description>
+        </Product>
+        <Product>
+            <主標>
+                <![CDATA[Ecobook]]>
+            </主標>
+            <副標>
+                <![CDATA[Notebook]]>
+            </副標>
+            <小圖連結>
+                <![CDATA[uploadfile/006.jpg]]>
+            </小圖連結>
+            <大圖連結>
+                <![CDATA[uploadfile/_l_006.jpg]]>
+            </大圖連結>
+            <Name>
+                <![CDATA[Ecobook]]>
+            </Name>
+            <Categories>
+                <![CDATA[Notebook]]>
+            </Categories>
+            <DATE>
+                <![CDATA[Sensual Poetry]]>
+            </DATE>
+            <Description>
+                <![CDATA[To look and act outside the box, we rigorously explore, experiment and implement techniques that achieve breath-taking results.]]>
+            </Description>
+        </Product>
+        <Product>
+            <主標>
+                <![CDATA[Ecobook]]>
+            </主標>
+            <副標>
+                <![CDATA[Notebook]]>
+            </副標>
+            <小圖連結>
+                <![CDATA[uploadfile/007.jpg]]>
+            </小圖連結>
+            <大圖連結>
+                <![CDATA[uploadfile/_l_007.jpg]]>
+            </大圖連結>
+            <Name>
+                <![CDATA[Ecobook]]>
+            </Name>
+            <Categories>
+                <![CDATA[Notebook]]>
+            </Categories>
+            <DATE>
+                <![CDATA[Sensual Poetry]]>
+            </DATE>
+            <Description>
+                <![CDATA[To look and act outside the box, we rigorously explore, experiment and implement techniques that achieve breath-taking results.]]>
+            </Description>
+        </Product>
+        <Product>
+            <主標>
+                <![CDATA[R2]]>
+            </主標>
+            <副標>
+                <![CDATA[Notebook]]>
+            </副標>
+            <小圖連結>
+                <![CDATA[uploadfile/008.jpg]]>
+            </小圖連結>
+            <大圖連結>
+                <![CDATA[uploadfile/_l_008.jpg]]>
+            </大圖連結>
+            <Name>
+                <![CDATA[R2]]>
+            </Name>
+            <Categories>
+                <![CDATA[Notebook]]>
+            </Categories>
+            <DATE>
+                <![CDATA[]]>
+            </DATE>
+            <Description>
+                <![CDATA[]]>
+            </Description>
+        </Product>
+        <Product>
+            <主標>
+                <![CDATA[Lamborghini]]>
+            </主標>
+            <副標>
+                <![CDATA[Notebook]]>
+            </副標>
+            <小圖連結>
+                <![CDATA[uploadfile/009.jpg]]>
+            </小圖連結>
+            <大圖連結>
+                <![CDATA[uploadfile/_l_001.jpg]]>
+            </大圖連結>
+            <Name>
+                <![CDATA[Lamborghini]]>
+            </Name>
+            <Categories>
+                <![CDATA[Notebook]]>
+            </Categories>
+            <DATE>
+                <![CDATA[Embodying Classic Brand]]>
+            </DATE>
+            <Description>
+                <![CDATA[To capture the spirit and power of Lamborghini, we studied their line of cars extensively. The fine detailing of this notebook is on par with an artfully constructed sports car. The visual language of the air vents emulates a wheel cover and the same bold stitches on a car seat have been implemented on the palm rest.]]>
+            </Description>
+        </Product>
+        <Product>
+            <主標>
+                <![CDATA[W1]]>
+            </主標>
+            <副標>
+                <![CDATA[Notebook]]>
+            </副標>
+            <小圖連結>
+                <![CDATA[uploadfile/010.jpg]]>
+            </小圖連結>
+            <大圖連結>
+                <![CDATA[uploadfile/_l_002.jpg]]>
             </大圖連結>
             <Name>
                 <![CDATA[W1]]>
